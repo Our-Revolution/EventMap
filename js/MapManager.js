c7b33891043ada53c9191365960d0bb29414df10
bahoo <jon.c.culver@gmail.com>
2017-02-21 10:15:04 -0800

    Feb 25th up in this.

--- js/MapManager.js
+++ js/MapManager.js
@@ -293,6 +293,10 @@ var MapManager = (function($, d3, leaflet) {
             L.circleMarker(item.latLng, { radius: 12, className: item.className, color: 'white', fillColor: '#F55B5B', opacity: 0.8, fillOpacity: 0.7, weight: 2 })
               .on('click', function(e) { _popupEvents(e); })
               .addTo(overlays);
+          }else if(item.className.match(/feb\-25th/ig)){
+            L.circleMarker(item.latLng, { radius: 7, className: item.className, color: 'white', fillColor: '#F55B5B', opacity: 0.8, fillOpacity: 0.7, weight: 2 })
+              .on('click', function(e) { _popupEvents(e); })
+              .addTo(overlays);
           } else {
 
             L.circleMarker(item.latLng, { radius: 5, className: item.className, color: 'white', fillColor: '#1462A2', opacity: 0.8, fillOpacity: 0.7, weight: 2 })
